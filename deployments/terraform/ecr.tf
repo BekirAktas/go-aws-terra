16f5df8ac9691fe1d078826a9a9ddb811deb6373
Bekir Aktas <bekir.aktas@hubx.co>
2024-05-12 05:34:18 +0300

    refactor: image push commands

--- deployments/terraform/ecr.tf
+++ deployments/terraform/ecr.tf
@@ -1,5 +1,14 @@
-resource "aws_ecr_repository" "go-aws-terra-image-repo" {
-  name = "go-aws-terra-image-repo"
+resource "aws_ecr_repository" "go-aws-terra-authors-image-repo" {
+  name = "go-aws-terra-authors-image-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "go-aws-terra-telegram-image-repo" {
+  name = "go-aws-terra-telegram-image-repo"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
