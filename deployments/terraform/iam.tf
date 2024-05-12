6e0fc88d0928ce7b366eb4ed12b5b7894a16eecf
Bekir Aktas <bekir.aktas@hubx.co>
2024-05-12 04:45:29 +0300

    feat: change cli

--- deployments/terraform/iam.tf
+++ deployments/terraform/iam.tf
@@ -27,4 +27,11 @@ resource "aws_iam_role_policy_attachment" "ec2-container-registry" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_user" "ecr-admin-circleci" {
+  name = "ecr-admin-circleci"
+}
 
+resource "aws_iam_user_policy_attachment" "ecr-admin-circleci-policy" {
+  user       = aws_iam_user.ecr-admin-circleci.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
